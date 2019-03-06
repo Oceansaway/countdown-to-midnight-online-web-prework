261215d32bd5f86ba34a2df3d30205c235f7b12b
Stephanie Purple <spurple133@gmail.com>
2019-03-06 17:37:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,13 @@ def countdown(number)
 end
 "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(num)
+  
+    while num > 0
+      puts "#{num} SECOND(S)!"
+    num -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
+  end
